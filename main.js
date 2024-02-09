079c008baa2cd1c06946cc3d55cd7631c1187e94
Seïn Bucher <sein.bucher@gmail.com>
2024-02-10 00:30:00 +0100

    im so tired

--- main.js
+++ main.js
@@ -2,5 +2,21 @@ const timeNode = document.querySelector('.time');
 
 setInterval(() => {
     const time = new Date();
-    timeNode.textContent = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
-}, 1000);
+    timeNode.textContent = formatTime(time);
+}, 1000)
+
+function formatTime(time) {
+    let hours;
+    let minutes;
+    let seconds;
+    if (time.getHours()>=10) {hours = `${time.getHours()}`} else 
+    if (time.getHours()<10 && time.getHours>0) {hours = `0${time.getHours()}`} else
+    {hours = '00'}
+    if (time.getMinutes()>=10) {minutes = `${time.getMinutes()}`} else 
+    if (time.getMinutes()<10 && time.getMinutes>0) {minutes = `0${time.getMinutes()}`} else
+    {minutes = '00'}
+    if (time.getSeconds()>=10) {seconds = `${time.getSeconds()}`} else 
+    if (time.getSeconds()<10 && time.getSeconds>0) {seconds = `0${time.getSeconds()}`} else
+    {seconds = '00'}
+    return hours+':'+minutes+':'+seconds;
+}
