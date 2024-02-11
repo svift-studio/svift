bef084fb3be7f9135525a72f99760ef49c1ef355
Seïn Bucher <sein.bucher@gmail.com>
2024-02-11 22:01:56 +0100

    pricing calculator

---
+++ calculator.js
@@ -0,0 +1,152 @@
+const websiteIncluded = ["Unique Customized Design",
+"Prototyping & Testing",
+"Target Market Analysis",
+"SEO Implementation",
+"Analytics Integration",
+"Site Speed Optimization",
+"Cross-Platform Responsive",
+"HTML5, CSS3 & JavaScript",
+"WCAG & ADA Compliant",
+"Satisfaction Guarantee"]
+const maintenanceIncluded = ["Functionality Checks",
+"Dependency Updates",
+"Site Analytics & Customer Data",
+"Uptime Monitoring",
+"Domain & SSL Renewals",
+"Priority Bug Fixing",
+"Backup Management",
+"Browser and Device Testing",
+"Site Speed Optimization"]
+const marketingIncluded = ["Functionality Checks",
+"Dependency Updates",
+"Site Analytics & Customer Data",
+"Uptime Monitoring",
+"Domain & SSL Renewals",
+"Priority Bug Fixing",
+"Backup Management",
+"Browser and Device Testing",
+"Site Speed Optimization"]
+
+const animationSpeed = 0; // in ms
+
+const websiteContent = document.querySelector('.included-website .included-content');
+const maintenanceContent = document.querySelector('.included-maintenance .included-content');
+const marketingContent = document.querySelector('.included-marketing .included-content');
+
+const includedHeader = document.querySelector('.included-header')
+const includedBtn = document.querySelector('.included-btn')
+
+const calculator = document.querySelector('.calculator')
+const pagesSlider = document.querySelector('.pages-slider')
+const priceOnceNode = document.querySelector('.price-once')
+const priceMonthlyNode = document.querySelector('.price-monthly')
+const deliveryNode = document.querySelector('.delivery')
+const pageNumberNode = document.querySelector('.page-number')
+const checkboxes = document.querySelector('.checkboxes-content')
+const checkboxItems = document.querySelectorAll('.checkbox-item')
+
+const basePriceWebsite = 10000;
+const basePriceMaintenance = 500;
+const basePriceMarketing = 4900;
+
+const costPerPageWebsite = 11000;
+const costPerPageMaintenance = 1200;
+
+// 1=day, 0.1=2.4h
+const baseTime = 1; 
+const timePerPage = 1;
+
+let priceOnce;
+let priceMonthly;
+let deliveryTime;
+
+let website = 1;
+let maintenance = 1;
+let marketing = 0;
+let pages = 3;
+updateCalc()
+
+includedItems('website')
+includedItems('maintenance')
+includedItems('marketing')
+function updateCalc() {
+    pages = pagesSlider.value;
+    priceOnce = 
+        website*(basePriceWebsite+(costPerPageWebsite*pages)); 
+    priceMonthly = 
+        (maintenance*(basePriceMaintenance+(costPerPageMaintenance*pages)))+
+        (marketing*basePriceMarketing);
+    deliveryTime = baseTime+website*(timePerPage*pages);
+    priceOnceNode.textContent = addSpace(priceOnce.toFixed());
+    priceMonthlyNode.textContent = addSpace(priceMonthly.toFixed());
+    deliveryNode.textContent = deliveryTime.toFixed();
+    pageNumberNode.textContent = pages;
+}
+
+checkboxes.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('checkbox-item')) return;
+    e.target.classList.toggle('active')
+
+    if (e.target.id === 'website') {
+        website===0 ? website=1 : website=0;
+    }
+    if (e.target.id === 'maintenance') maintenance===0 ? maintenance=1 : maintenance=0;
+    if (e.target.id === 'marketing') marketing===0 ? marketing=1 : marketing=0;
+    
+    updateCalc()
+    includedItems(e.target.id)
+})
+
+includedBtn.addEventListener('click', () => {
+    if (includedBtn.classList.contains('active')) {
+        includedBtn.textContent = `See what's included +`;
+        includedHeader.style.maxHeight = '0px'
+    } else {
+        includedBtn.textContent = `Hide what's included -`;
+        includedHeader.style.maxHeight = '600px'
+    }
+    includedBtn.classList.toggle('active')
+})
+
+function includedItems(element) {
+    switch (element) {
+        case 'website':
+            website === 1 ? addElements(websiteIncluded, websiteContent) : removeElements(websiteContent);
+            break;
+        case 'maintenance':
+            maintenance === 1 ? addElements(maintenanceIncluded, maintenanceContent) : removeElements(maintenanceContent);
+            break;
+        case 'marketing':
+            marketing === 1 ? addElements(marketingIncluded, marketingContent) : removeElements(marketingContent);
+            break;
+    }
+}
+
+function addElements(content, container) {
+    let timeout = 0;
+    content.map(element => {
+        let newItem = document.createElement('div');
+        newItem.classList.add('included-item')
+        newItem.textContent = element;
+        setTimeout(() => {
+            container.appendChild(newItem);
+        }, timeout);
+        timeout+=animationSpeed;
+    })
+}
+
+function removeElements(container) {
+    let timeout = 0;
+    container.querySelectorAll('.included-item').forEach(element => {
+        setTimeout(() => {
+            element.remove()            
+        }, timeout);
+        timeout+=animationSpeed;
+    })
+}
+
+function addSpace(num) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+pagesSlider.onchange = () => updateCalc();
--- main.js
+++ main.js
@@ -1,23 +1,5 @@
 const timeNode = document.querySelector('.time');
 
-const modal = document.querySelector('#modal')
-const modalBtn = document.querySelector('#modal-btn')
-
-const modalContent = document.querySelector('.modal-pages');
-const modalPages = document.querySelectorAll('.modal-page');
-
-const modalClose = document.querySelector('.modal-close');
-const modalPrevious = document.querySelector('.modal-previous');
-const modalNext = document.querySelector('.modal-next');
-
-const modalInputWebsite = document.querySelector('#modal-input-website');
-const modalInputEmail = document.querySelector('#modal-input-email');
-
-const error = document.querySelector('.error');
-
-let modalPageWidth = 100/modalPages.length;
-let modalPage = 0;
-
 setInterval(() => {
     const time = new Date();
     timeNode.textContent = formatTime(time);
@@ -27,62 +9,4 @@ function formatTime(time) {
     return time.getHours().toString().padStart(2, '0') + ':' +
     time.getMinutes().toString().padStart(2, '0') + ':' +
     time.getSeconds().toString().padStart(2, '0');
-}
-
-modalBtn.addEventListener('click', () => {
-    modal.style.display = 'flex';
-    
-    setTimeout(() => {
-        modal.style.opacity = '1';
-    }, 150);
-})
-
-modalClose.addEventListener('click', () => {
-    modal.style.opacity = '0';
-    setTimeout(() => {
-        modal.style.display = 'none';
-        error.style.display = 'none';
-        error.textContent = '';
-    }, 150);
-})
-
-modalNext.addEventListener('click', () => {
-    if (inputCheck(modalPages[modalPage].querySelector('input'))) return;
-    error.style.display = 'none';
-    error.textContent = '';
-
-    if (modalPage<modalPages.length-1) modalPage++;
-    if (modalPage>0) modalPrevious.style.display = 'block';
-    if (modalPage===modalPages.length-1) modalNext.textContent = 'submit';
-    modalContent.style.transform = `translateX(-${modalPage*modalPageWidth}%)`
-})
-
-modalPrevious.addEventListener('click', () => {
-    error.style.display = 'none';
-    error.textContent = '';
-    if (modalPage>0) modalPage--;
-    if (modalPage===0) modalPrevious.style.display = 'none';
-    if (modalPage<modalPages.length-1) modalNext.textContent = 'next';
-    modalContent.style.transform = `translateX(-${modalPage*modalPageWidth}%)`
-})
-
-
-function inputCheck(page) {
-    console.log(page.className)
-    switch (page.className) {
-        case 'website':
-            if (page.value.length<3 || !page.value.includes('.')) {
-                error.textContent = 'Enter your Website!';
-                error.style.display = 'flex';
-                return true;
-            }
-            break;
-        case 'email':
-            if (page.value.length<3 || !page.value.includes('.') || !page.value.includes('@')) {
-                error.textContent = 'Enter your Email!';
-                error.style.display = 'flex';
-                return true;
-            }    
-            break;
-    }
 }
